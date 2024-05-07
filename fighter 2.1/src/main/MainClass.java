62430cbf23590e04baa875b2165ce99523ef947f
himanshukumar7227 <157729399+himanshukumar7227@users.noreply.github.com>
2024-05-07 13:23:48 +0530

    Delete fighter 2.1 directory

--- fighter 2.1/src/main/MainClass.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class MainClass {
-    public static void main(String[] args) {
-        new Game();// this is creation of game class due to this
-        // command constructor will be called of class "Game.java"
-
-    }
-}
