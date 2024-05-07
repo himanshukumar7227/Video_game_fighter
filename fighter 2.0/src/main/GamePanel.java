0c929f3e2c42be40be23cc2a6c2d8f55aa2b6286
himanshukumar7227 <157729399+himanshukumar7227@users.noreply.github.com>
2024-05-07 13:44:53 +0530

    Add files via upload

--- fighter 2.0/src/main/GamePanel.java
+++ fighter 2.0/src/main/GamePanel.java
@@ -1,35 +1,37 @@
 package main;
 import inputs.MouseInputs;
 import inputs.keyboardInputs;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;//Jpannel class importing important for drawing stufs
 //into the window.
 import java.awt.*;//for graphics
-import java.awt.event.KeyEvent;//importing module for controling the
-//the input.
-import java.awt.event.KeyListener;//importing module for controling the
-//the input.
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GamePanel extends JPanel{
     //Jpanel is used to create the game internal stuf all we see.
     private MouseInputs mouseInputs;//instanciating MouseInputs object.
+    private keyboardInputs keyboardInputs;
     private int frames=0;//initializing frame.
     private long lastCheck=0;//this is used to check the time
     //if not done it will show time from very begning and will not
     //reset.
     private float xDelta=100,yDelta=100;//this is the position of rectangle.
     // At the starting of the loop.
-    private float xDir=0.9f,yDir=0.8f;//this is for increamenting
-    // 'xDelta' and 'yDelta'.
-    private Color color=new Color(24, 150, 20);//creating object of
-    //color class for use of differnet colours of the rectangle.
-    private Random random;//it for random integers or for random value of
-    //colours.
+
+    private BufferedImage img,subImg;
+
     public GamePanel(){//Gamepanel constructor
-        random= new Random();//random module for randowm values of colour codes
-        //as you can see in the rectangle
+
         mouseInputs= new MouseInputs(this);//calling mouse inputs
         //here 'this' is for gamePanel as we have used an argument in mouse input
+        keyboardInputs =new keyboardInputs(this);//calling keyboard inputs
+
+        importImage();//from image importing method.
+        setPannelSize();//creating window.
+
         addKeyListener(new keyboardInputs(this));//this helps in taking
         //input from keyboard.
         addMouseListener(mouseInputs);//this helps in taking input from
@@ -37,6 +39,26 @@ public class GamePanel extends JPanel{
         addMouseMotionListener(mouseInputs);//this is used for motion detection.
 
     }
+
+    private void importImage() {
+        /* This method help us to import or bring image or involve images in the
+        programme.*/
+        InputStream is= getClass().getResourceAsStream("/player_sprites.png");
+        try {
+            img= ImageIO.read(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setPannelSize() {
+        /* This method enable us the create a window on the screen of specific size.*/
+        Dimension size=new Dimension(920,520);
+        setMinimumSize(size);
+        setMaximumSize(size);
+        setPreferredSize(size);
+    }
+
     public void changexDelta(int value){
         //this method is used to change the value or position of
         //'xDeta' or position of rectangle so that it looks as rectangle
@@ -61,43 +83,15 @@ public class GamePanel extends JPanel{
     public void paintComponent(Graphics g){
         super.paintComponent(g);//it is predifined method which is
         //override by us to create image in Game Pannel.
-        g.setColor(color);//this the predefind method used to change colour of
-        //rectangle in the gamepannel.
-        g.fillRect((int)xDelta,(int)yDelta,200,30);//this will draw a rectangele.
 
-        updateRectangle();// this for updating the position of rectangle
+        subImg=img.getSubimage(1*64,7*40,64,40);//this will extract the
+        //individual images of player
 
+        g.drawImage(subImg,(int)xDelta,(int)yDelta,120,80,null);// this will draw
+        //the image of the player in gamepannel at specific location.
 
     }
 
-    private void updateRectangle() {
-        // this method is for updating the position of rectangle in
-        //loop
-        xDelta+=xDir;
-        if(xDelta>400||xDelta<0){
-            //if rectangle goes out of pannel or goes to xposition of
-            //0 then we will multiply that position with -1 and and change the
-            //colour of rectangle here
-            xDir*=-1;
-            color = getRndColor();
-        }
-        yDelta+=yDir;
-        if(yDelta>400||yDelta<0) {
-            //if rectangle goes out of pannel or goes to yposition of
-            //0 then we will multiply that position with -1 and and change the
-            //colour of rectangle here
-            yDir *= -1;
-            color = getRndColor();
-        }
-    }
 
-    private Color getRndColor() {
-        // here we are asignin random r g b vlaue which is later going
-        //to used to seen in rectangle when there is specific condition is
-        //met.
-        int r=random.nextInt(255);
-        int b=random.nextInt(255);
-        int g=random.nextInt(255);
-        return new Color(r,b,g);
-    }
+
 }
--- fighter 2.0/src/main/Game_window.java
+++ fighter 2.0/src/main/Game_window.java
@@ -5,9 +5,14 @@ public class Game_window {
     public  Game_window(GamePanel gamePanel){
 
         jframe = new JFrame();// important for initialization of window.
-        jframe.setSize(400,400);//This will set size of the window.
+
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Due to this as we close the game window programme will be terminated.
         jframe.add(gamePanel); // this will add gamepannel stuf in the window.
+
+        jframe.setResizable(false);//this will disable the featuere of window
+        //maximizing and minimizing
+        jframe.pack();//this will help us to create frame across the gamepannel.
+
         jframe.setLocationRelativeTo(null); // this will make main window appear in center of screen.
         jframe.setVisible(true);//this helps in making window visible.
     }
